823fa98ec6ce31cf11d09a00e25c79c1f98ba7ae
Nooralwachi <noor_alwachi@yahoo.com>
2021-04-16 19:13:55 -0700

    Update runtime.py

--- runtime.py
+++ runtime.py
@@ -7,18 +7,17 @@ def runtime(filename):
 			result = []
 			time,date, pid, status = line.split()
 			process_time = datetime.strptime(time+ ' '+date, '%H:%M:%S %m/%d/%Y')
-			print(process_time, pid, status)
+			#print(process_time, pid, status)
 			if status == 'start':
 				process[pid] =[status, process_time, timedelta(0)]			
 			elif status == 'pause' and pid in process.keys():
-				print(process)
+				#print(process)
 				process[pid] =[status, process_time, process_time - process[pid][1]]
 			elif status == 'resume' and pid in process.keys():
-				print(process)
+				#print(process)
 				process[pid][0],process[pid][1] =status,process_time
 			elif status == 'exit' and pid in process.keys():
 				time = process_time- process[pid][1] 
 				process[pid] =[status, process_time,process[pid][2] +time]
 				print(pid,str(process[pid][2]))
-				#print(pid, datetime.fromtimestamp(process[pid][2]))	
 runtime('pid.txt')
